0c528297bcfa05b6be164903031daa85702c8bdc
Pavel Král <pavel.kral@gmail.com>
2023-12-21 11:27:23 +0100

    wip3

--- home/pkral.nix
+++ home/pkral.nix
@@ -269,6 +269,26 @@ in
     zim
   ];
 
+  xsession = {
+  enable = true;
+  pointerCursor = {
+    size = 40;
+    package = pkgs.nur.repos.ambroisie.vimix-cursors;
+    name = "Vimix-white-cursors";
+    # name = "Vimix-cursors";
+
+    # package = pkgs.capitaine-cursors;
+    # name = "capitaine-cursors";
+
+    # package = pkgs.nur.repos.ambroisie.volantes-cursors;
+    # name = "volantes_light_cursors";
+    # name = "volantes_cursors";
+
+    # package = pkgs.nur.repos.dan4ik605743.lyra-cursors;
+    # name = "LyraF-cursors";
+  };
+};
+
 }
 
 # config:
